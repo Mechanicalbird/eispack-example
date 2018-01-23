e6610b101cbd77cd5f2a2e282b0dff172dd53fdc
Mechanicalbird <mechanicalbird_just@yahoo.com>
2018-01-23 02:53:42 -0800

    Create eispack_ex5.f

---
+++ eispack_ex5.f
@@ -0,0 +1,81 @@
+*  =============================================================================
+*
+*     .. Parameters ..
+      INTEGER          N
+      PARAMETER        ( N = 3 )
+      INTEGER          LDA
+      PARAMETER        ( LDA = N )
+      INTEGER          LWMAX
+      PARAMETER        ( LWMAX = 1000 )
+*
+*     .. Local Scalars ..
+      INTEGER          INFO, LWORK
+*
+*     .. Local Arrays ..
+      DOUBLE PRECISION A( LDA, N ), W( N ), WORK( LWMAX )
+      DATA             A/
+     $  0.0, 1.0, 0.0,
+     $ -0.0, 0.0, 0.3999999,
+     $ -0.0, 283640.0, 0.0
+     $                  /
+*
+*     .. External Subroutines ..
+      EXTERNAL         DSYEV
+      EXTERNAL         PRINT_MATRIX
+*
+*     .. Intrinsic Functions ..
+      INTRINSIC        INT, MIN
+*
+*     .. Executable Statements ..
+      WRITE(*,*)'DSYEV Example Program Results'
+*
+*     Query the optimal workspace.
+*
+      LWORK = -1
+      CALL DSYEV( 'Vectors', 'Upper', N, A, LDA, W, WORK, LWORK, INFO )
+      LWORK = MIN( LWMAX, INT( WORK( 1 ) ) )
+*
+*     Solve eigenproblem.
+*
+      CALL DSYEV( 'Vectors', 'Upper', N, A, LDA, W, WORK, LWORK, INFO )
+*
+*     Check for convergence.
+*
+      IF( INFO.GT.0 ) THEN
+         WRITE(*,*)'The algorithm failed to compute eigenvalues.'
+         STOP
+      END IF
+*
+*     Print eigenvalues.
+*
+      CALL PRINT_MATRIX( 'Eigenvalues', 1, N, W, 1 )
+*
+*     Print eigenvectors.
+*
+      CALL PRINT_MATRIX( 'Eigenvectors (stored columnwise)', N, N, A,
+     $                   LDA )
+      STOP
+      END
+*
+*     End of DSYEV Example.
+*
+*  =============================================================================
+*
+*     Auxiliary routine: printing a matrix.
+*
+      SUBROUTINE PRINT_MATRIX( DESC, M, N, A, LDA )
+      CHARACTER*(*)    DESC
+      INTEGER          M, N, LDA
+      DOUBLE PRECISION A( LDA, * )
+*
+      INTEGER          I, J
+*
+      WRITE(*,*)
+      WRITE(*,*) DESC
+      DO I = 1, M
+         WRITE(*,9998) ( A( I, J ), J = 1, N )
+      END DO
+*
+ 9998 FORMAT( 11(:,1X,F6.2) )
+      RETURN
+      END
